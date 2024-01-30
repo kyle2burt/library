f89b9234a190d7fb43490de66cf81ee67189e210
Kyle Burt <kyle2burt@gmail.com>
2024-01-29 19:07:36 -0800

    Add JS to display book array onto page

--- javascript.js
+++ javascript.js
@@ -1,4 +1,5 @@
 const myLibrary = []
+const pageContent = document.querySelector(".content");
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -14,15 +15,48 @@ function Book(title, author, pages, read) {
 }
 
 function addBook(title, author, pages, read) {
-    const newBook = new Book(title, author, pages, read)
+    const newBook = new Book(title, author, pages, read);
     myLibrary.push(newBook);
 }
 
-addBook("The Hunger Games", "Some Lady", 432, true)
-addBook("The Big Fish", "Some Guy", 234, false)
+addBook("The Hunger Games", "Some Lady", 432, true);
+addBook("The Big Fish", "Some Guy", 234, false);
 
 function displayAllBooks() {
     myLibrary.forEach((book) => {
-        console.log(book.title);
+        const bookCard = document.createElement("div");
+        const bookImage = document.createElement("img");
+        const bookTitle = document.createElement("h3");
+        const bookInfo = document.createElement("div");
+        const bookAuthor = document.createElement("p");
+        const bookPages = document.createElement("p");
+        const bookRead = document.createElement("button");
+        const bookRemove = document.createElement("button");
+
+        bookCard.classList.add("book");
+        bookInfo.classList.add("book-info");
+
+        bookImage.src = "icons/book.svg";
+        bookImage.setAttribute("style", "width: 150px");
+
+        bookTitle.textContent = book.title;
+        bookAuthor.textContent = book.author;
+        bookPages.textContent = book.pages + " pg";
+
+        bookRead.textContent = "Read";
+        bookRemove.textContent = "Remove";
+
+        bookInfo.appendChild(bookAuthor);
+        bookInfo.appendChild(bookPages);
+
+        bookCard.appendChild(bookImage);
+        bookCard.appendChild(bookTitle);
+        bookCard.appendChild(bookInfo);
+        bookCard.appendChild(bookRead);
+        bookCard.appendChild(bookRemove);
+        
+        pageContent.appendChild(bookCard);
     });
 }
+
+displayAllBooks();
