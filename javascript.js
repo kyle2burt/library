8d0174ff885a73d89bd3c70de23567e584e5b1d8
Kyle Burt <kyle2burt@gmail.com>
2024-02-12 15:45:12 -0800

    Refactor: use classes instead of constructors

--- javascript.js
+++ javascript.js
@@ -1,100 +1,133 @@
-const myLibrary = []
-const pageContent = document.querySelector(".content");
-const dialog = document.querySelector("dialog");
-const addBookButton = document.querySelector(".add-book")
-const cancel = document.querySelector("#cancel");
-const submit = document.querySelector("#submit");
+class Book {
+    constructor(title, author, pages, read) {
+        this.title = title;
+        this.author = author;
+        this.pages = pages;
+        this.read = read;
+        this.readString = this.#getReadString();
+    }
 
-function Book(title, author, pages, read) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.read = read;
-    this.readString = function() {
+    #getReadString() {
         let readString = "Read";
-        if(!this.read) readString = "Not Read Yet";
-        
+        if (!this.read) readString = "Not Read Yet";
+
         return readString;
     }
 }
 
-function addBook(title, author, pages, read) {
-    const newBook = new Book(title, author, pages, read);
-    myLibrary.push(newBook);
+class Library {
+    constructor(bookList) {
+        this.#books = bookList || [];
+        this.#displayAllBooks();
+    }
+
+    #books;
+
+    addBook(title, author, pages, read) {
+        this.newBook = new Book(title, author, pages, read);
+        this.#books.push(this.newBook);
+        this.#displayBook(this.#books[this.#books.length - 1])
+    }
+
+    #displayBook(book) {
+        const pageContent = document.querySelector(".content");
+        const addBookButton = document.querySelector(".add-book")
+        const bookCard = document.createElement("div");
+        const bookImage = document.createElement("img");
+        const bookTitle = document.createElement("h3");
+        const bookInfo = document.createElement("div");
+        const bookAuthor = document.createElement("p");
+        const bookPages = document.createElement("p");
+        const bookRead = document.createElement("button");
+        const bookRemove = document.createElement("button");
+
+        bookCard.classList.add("book");
+        bookInfo.classList.add("book-info");
+
+        bookImage.src = "icons/book.svg";
+        bookImage.setAttribute("style", "width: 150px");
+
+        bookTitle.textContent = book.title;
+        bookAuthor.textContent = book.author;
+        bookPages.textContent = book.pages + " pg";
+
+        bookRead.textContent = book.readString;
+        bookRemove.textContent = "Remove";
+
+        bookInfo.appendChild(bookAuthor);
+        bookInfo.appendChild(bookPages);
+
+        bookCard.appendChild(bookImage);
+        bookCard.appendChild(bookTitle);
+        bookCard.appendChild(bookInfo);
+        bookCard.appendChild(bookRead);
+        bookCard.appendChild(bookRemove);
+
+        pageContent.insertBefore(bookCard, addBookButton);
+
+        bookRead.addEventListener("click", () => {
+            if (book.read) book.read = false;
+            else book.read = true;
+
+            bookRead.textContent = book.getReadString();
+        });
+
+        bookRemove.addEventListener("click", () => {
+            books.splice(books.indexOf(book), 1);
+            bookCard.remove();
+        });
+    }
+
+    #displayAllBooks() {
+        this.#books.forEach((book) => {
+            this.#displayBook(book);
+        });
+    }
 }
 
-function displayBook(book) {
-    const bookCard = document.createElement("div");
-    const bookImage = document.createElement("img");
-    const bookTitle = document.createElement("h3");
-    const bookInfo = document.createElement("div");
-    const bookAuthor = document.createElement("p");
-    const bookPages = document.createElement("p");
-    const bookRead = document.createElement("button");
-    const bookRemove = document.createElement("button");
+class Modal {
+    constructor() {
+        Modal.#addBookButton();
+        Modal.#cancelButton();
+        Modal.#submitButton();
+    }
 
-    bookCard.classList.add("book");
-    bookInfo.classList.add("book-info");
+    static #dialog = document.querySelector("dialog");
 
-    bookImage.src = "icons/book.svg";
-    bookImage.setAttribute("style", "width: 150px");
+    static #addBookButton() {
+        const addBookButton = document.querySelector(".add-book")
+        addBookButton.addEventListener("click", () => {
+            Modal.#dialog.showModal();
+        });
+    }
 
-    bookTitle.textContent = book.title;
-    bookAuthor.textContent = book.author;
-    bookPages.textContent = book.pages + " pg";
+    static #cancelButton() {
+        const cancel = document.querySelector("#cancel");
+        cancel.addEventListener("click", () => {
+            Modal.#dialog.close();
+        });
+    }
 
-    bookRead.textContent = book.readString();
-    bookRemove.textContent = "Remove";
-
-    bookInfo.appendChild(bookAuthor);
-    bookInfo.appendChild(bookPages);
-
-    bookCard.appendChild(bookImage);
-    bookCard.appendChild(bookTitle);
-    bookCard.appendChild(bookInfo);
-    bookCard.appendChild(bookRead);
-    bookCard.appendChild(bookRemove);
-
-    pageContent.insertBefore(bookCard, addBookButton);
-
-    bookRead.addEventListener("click", () => {
-        if (book.read) book.read = false;
-        else book.read = true;
-
-        bookRead.textContent = book.readString();
-    });
-
-    bookRemove.addEventListener("click", () => {
-        myLibrary.splice(myLibrary.indexOf(book), 1);
-        bookCard.remove();
-    });
+    static #submitButton() {
+        const submit = document.querySelector("#submit");
+        submit.addEventListener("click", (event) => {
+            const Title = document.querySelector("#title");
+            const Author = document.querySelector("#author");
+            const Pages = document.querySelector("#pages");
+            const Read = document.querySelector("#read")
+            event.preventDefault();
+            myLibrary.addBook(Title.value, Author.value, Pages.value, Read.value);
+            Modal.#dialog.close();
+        });
+    }
 }
 
-function displayAllBooks() {
-    myLibrary.forEach((book) => {
-        displayBook(book);
-    });
-}
+let bookList = [
+    new Book("The Hunger Games", "Some Lady", 432, true),
+    new Book("The Big Fish", "Some Guy", 234, false)
+];
 
-addBookButton.addEventListener("click", () => {
-    dialog.showModal();
-});
+let myLibrary = new Library(bookList);
+let myModal = new Modal();
 
-cancel.addEventListener("click", () => {
-    dialog.close();
-});
-
-submit.addEventListener("click", (event) => {
-    const Title = document.querySelector("#title");
-    const Author = document.querySelector("#author");
-    const Pages = document.querySelector("#pages");
-    const Read = document.querySelector("#read")
-    event.preventDefault();
-    addBook(Title.value, Author.value, Pages.value, Read.value);
-    displayBook(myLibrary[myLibrary.length - 1]);
-    dialog.close();
-});
-
-addBook("The Hunger Games", "Some Lady", 432, true);
-addBook("The Big Fish", "Some Guy", 234, false);
-displayAllBooks();
+myLibrary.addBook("Another Book", "Another Author", 42, true);
