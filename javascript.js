684b006a9d983da865b58f9bbdc663d4a8742329
Kyle Burt <arrant-pettier2u@icloud.com>
2025-11-10 00:57:51 -0800

    Add drawBooks function to display library on page

--- javascript.js
+++ javascript.js
@@ -7,9 +7,13 @@ function Book(title, author, pages, read) {
     this.read = read;
     this.id = crypto.randomUUID();
 
-    this.info = function () {
-        if (read) return `${this.title} by ${this.author}, ${pages} pages, read`;
-        else return `${this.title} by ${this.author}, ${pages} pages, not read yet`;
+    this.info = function() {
+        return `${this.title} by ${this.author}, ${pages} pages, ${read}`;
+    }
+
+    this.readText = function() {
+        if (read) return 'Read';
+        else return 'Not read';
     }
 }
 
@@ -18,6 +22,38 @@ function addBookToLibrary(title, author, pages, read) {
     myLibrary.push(book);
 }
 
+function drawBooks() {
+    const pageLibrary = document.querySelector('.library');
+    myLibrary.forEach((book) => {
+        const title = document.createElement('h4');
+        title.classList.add('title');
+        title.textContent = book.title;
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", 295, false);
+        const author = document.createElement('p');
+        author.classList.add('author');
+        author.textContent = book.author;
+
+        const pages = document.createElement('p');
+        pages.classList.add('pages');
+        pages.textContent = book.pages;
+
+        const read = document.createElement('p');
+        read.classList.add('read');
+        read.textContent = book.readText();
+
+        const bookElem = document.createElement('div');
+        bookElem.classList.add('book');
+        bookElem.appendChild(title);
+        bookElem.appendChild(author);
+        bookElem.appendChild(pages);
+        bookElem.appendChild(read);
+
+        pageLibrary.appendChild(bookElem);
+
+    });
+}
+
+
+addBookToLibrary('The Hobbit', 'J.R.R. Tolkien', 295, false);
+drawBooks();
 console.log(myLibrary);
